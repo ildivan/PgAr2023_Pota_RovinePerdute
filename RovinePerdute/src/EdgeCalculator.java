60fbf506dc117911adb6ae8cae89e48f7d2878f8
Ivan Abrami <Ivanpitbull2512@gmail.com>
2023-05-31 13:22:09 +0200

    Commented EdgeCalculator, Metztli and Tonatiuh

--- RovinePerdute/src/EdgeCalculator.java
+++ RovinePerdute/src/EdgeCalculator.java
@@ -1,3 +1,9 @@
+/**
+ * Represents any class that can calculate the distance between two cities
+ */
 public interface EdgeCalculator {
-    public double calculateEdgeDistance(City first, City second);
+    /**
+     * Calculates the cost of traveling from a city to another, based on some concept of distance.
+     */
+    double calculateEdgeDistance(City first, City second);
 }
--- RovinePerdute/src/Tonatiuh.java
+++ RovinePerdute/src/Tonatiuh.java
@@ -1,5 +1,11 @@
+/**
+ * Class for a vehicle that calculates cost based of x and y coordinates, ignoring altitude
+ */
 public class Tonatiuh implements EdgeCalculator {
 
+    /**
+     * Calculates the cost between two cities based on x and y coordinates.
+     */
     @Override
     public double calculateEdgeDistance(City first, City second) {
         double deltaX =  Math.pow(second.getX() - first.getX(), 2);
