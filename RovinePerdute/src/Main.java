e7ac70fb49d573894966d37fbab56a5adead60eb
WitherTick <87392380+WitherTick@users.noreply.github.com>
2023-05-31 20:36:06 +0200

    feat: :children_crossing: command-line arguments
    Adds user interaction with command-line arguments.
    It support: --output, --files, --help commands.
    All commands have their own short version (-o, -f, -h).

--- RovinePerdute/src/Main.java
+++ RovinePerdute/src/Main.java
@@ -1,16 +1,71 @@
+import it.kibo.fp.lib.Menu;
+
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        do {
-            switch (LostRuinsMenu.mainMenu()) {
-                case 1 -> LostRuinsMenu.inputFilePath();
-                case 2 -> selectPreset();
-                case 0 -> {
-                    return;
+        Menu.clearConsole();
+
+        if (args.length == 0) {
+            do {
+                switch (LostRuinsMenu.mainMenu()) {
+                    case 1 -> LostRuinsMenu.inputFilePath();
+                    case 2 -> selectPreset();
+                    case 0 -> {
+                        return;
+                    }
                 }
+                Menu.clearConsole();
+            } while (true);
+        } else {
+            String directory = null;
+            ArrayList<String> listOfFiles = new ArrayList<>();
+            boolean fileList = false;
+            try {
+                for (int i=0; i<args.length; i++) {
+                    if (args[i].startsWith("-")) {
+                        switch (args[i].substring(args[i].lastIndexOf("-")+1)) {
+                            case "o":
+                            case "output":
+                                fileList = false;
+                                if (directory == null)
+                                    directory = args[++i];
+                                else
+                                    throw new IllegalArgumentException("Output directory cannot be specified multiple times!");
+                                break;
+                            case "f":
+                            case "files":
+                                fileList = true;
+                                break;
+                            case "h":
+                            case "help":
+                                if (args.length == 1) {
+                                    showHelp();
+                                    break;
+                                }
+                            default:
+                                throw new IllegalArgumentException("Invalid option: " + args[i].substring(args[i].lastIndexOf("-")+1));
+                        }
+                        continue;
+                    }
+                    if (fileList)
+                        listOfFiles.add(args[i]);
+                    else
+                        throw new IllegalArgumentException("You must specify -f command!");
+                }
+
+                if (directory != null && (!new File(directory).isDirectory() || !directory.endsWith("\\")))
+                    throw new IllegalArgumentException(String.format("Illegal argument: \"%s\" is not a directory!", directory));
+
+                for (String path : listOfFiles)
+                    startPathFinding(path, directory == null ? "Output/" : directory);
+
+            } catch (IllegalArgumentException i) {
+                System.err.println(i.getMessage());
             }
-        } while (true);
+        }
     }
 
     public static void startPathFinding(String sourcePath, String outputPath) {
@@ -19,6 +74,7 @@ public class Main {
             PathFinder p1 = new PathFinder(cities.get(0), cities.get(cities.size() - 1), new Tonatiuh());
             PathFinder p2 = new PathFinder(cities.get(0), cities.get(cities.size() - 1), new Metztli());
             XMLHandler.writeOutput(p1, p2, String.format(outputPath == null ? "RovinePerdute/output/Routes_%s" : outputPath+"Routes_%s", XMLHandler.getXMLFileName(sourcePath)));
+            Menu.clearConsole();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
@@ -36,5 +92,14 @@ public class Main {
                 return;
             }
         }
+    }
+
+    public static void showHelp() {
+        System.out.println(
+                "Usage: PgAr2023_Pota_RovinePerdute.jar (FILE_PATH)... [OUTPUT_FOLDER]\n\n"
+            +   "Option\tLong option\tMeaning\n"
+            +   "  -h\t--help\t\tShows this help message\n"
+            +   "  -f\t--files\t\tFollowed by all XML files to analyze\n"
+            +   "  -o\t--output\tDirectory for output files\n");
     }
 }
