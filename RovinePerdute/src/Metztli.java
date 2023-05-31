7e9d31daf458ad8d47121c92a1a6cb4ab9b8ffa9
Ivan Abrami <Ivanpitbull2512@gmail.com>
2023-05-31 13:22:38 +0200

    Commented Metztli

--- RovinePerdute/src/Metztli.java
+++ RovinePerdute/src/Metztli.java
@@ -1,5 +1,11 @@
+/**
+ * Class for a vehicle that calculates cost based of the altitude, or h value
+ */
 public class Metztli implements EdgeCalculator {
 
+    /**
+     * Calculates the cost between two cities based on h value
+     */
     @Override
     public double calculateEdgeDistance(City first, City second) {
         double fuel = second.getH() - first.getH();
