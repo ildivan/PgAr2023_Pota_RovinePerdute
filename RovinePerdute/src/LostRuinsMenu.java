6c9c33deed3780cf3e9e2bb90b71a7b78831c63e
WitherTick <87392380+WitherTick@users.noreply.github.com>
2023-05-31 23:35:56 +0200

    docs: :memo: doc for LostRuinsMenu.java

--- RovinePerdute/src/LostRuinsMenu.java
+++ RovinePerdute/src/LostRuinsMenu.java
@@ -2,8 +2,16 @@ import com.github.lalyos.jfiglet.FigletFont;
 import it.kibo.fp.lib.Menu;
 import it.kibo.fp.lib.InputData;
 
+/**
+ * The <strong>LostRuinsMenu</strong> class contains all the static methods
+ * for displaying contents to the user or for read the user input.
+ */
 public class LostRuinsMenu {
 
+    /**
+     * Menu for displaying the main functions.
+     * @return A number representing the user choice.
+     */
     public static int mainMenu() {
         Menu.clearConsole();
         welcome();
@@ -12,6 +20,10 @@ public class LostRuinsMenu {
         return tamaMenu.choose();
     }
 
+    /**
+     * Menu for displaying the preset paths.
+     * @return A number representing the user choice.
+     */
     public static int presetMenu() {
         Menu.clearConsole();
         String[] entries = { "5 cities", "13 cities", "50 cities", "200 cities", "2000 cities", "10000 cities" };
@@ -19,11 +31,18 @@ public class LostRuinsMenu {
         return presetMenu.choose();
     }
 
+    /**
+     * Function that read the file path input by the user.
+     */
     public static void inputFilePath() {
         String path = InputData.readNonEmptyString(Literals.INPUT_FILE_PATH_1, false);
         Main.startPathFinding(path, null);
     }
 
+    /**
+     * Shows a message to indicate to the user that the program is proceeding with the algorithm to find the best route.
+     * @param numOfCities The number of cities that are going to be calculated.
+     */
     public static void loading(int numOfCities) {
         try {
             if (numOfCities <= 2000)
